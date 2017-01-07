4ff159fe6cbb66728dbae065185f70b1b06b6dba
Mehow <Kaiser.mehow@gmail.com>
2017-01-07 13:17:34 +0000

    adding gaps between hexgons, showing both combined and non-combined sides for clarify

--- source/main.js
+++ source/main.js
@@ -29,33 +29,32 @@ let teams = [
 ];
 
 function build_hexagon_polygon(sideLength){
-    let corner_vertical = Math.sin(Math.PI/3)*sideLength;
-    let corner_horizontal = Math.cos(Math.PI/3)*sideLength;
-    let hexagonWidth = 2*sideLength;
-    let hexagonHeight = 2*corner_vertical;
+    let graphicSideLength = sideLength*0.6;
+    let corner_vertical = Math.sin(Math.PI/3)*graphicSideLength;
+    let corner_horizontal = Math.cos(Math.PI/3)*graphicSideLength;
     let polygon = new Phaser.Polygon(
-        new Phaser.Point(sideLength, 0),
+        new Phaser.Point(graphicSideLength, 0),
         new Phaser.Point(+corner_horizontal, +corner_vertical),
         new Phaser.Point(-corner_horizontal, +corner_vertical),
-        new Phaser.Point(-sideLength, 0),
+        new Phaser.Point(-graphicSideLength, 0),
         new Phaser.Point(-corner_horizontal, -corner_vertical),
         new Phaser.Point(+corner_horizontal, -corner_vertical)
     );
-    return {polygon: polygon, width: hexagonWidth, height: hexagonHeight};
+    return {polygon: polygon, height: 2*Math.sin(Math.PI/3)*sideLength};
 }
 
 function asign_sides(){
-    let sides = new Map();
+    let sides = [];
     for(let sideNumber = 0; sideNumber < 6; sideNumber++){
         //for non-random sides
-        sides.set(sideNumber, sideNumber%teams.length);
+        //sides.push(sideNumber%teams.length);
         //for random sides
-        //sides.set(sideNumber, Math.floor(Math.random()*teams.length));
+        sides.push(Math.floor(Math.random()*teams.length));
     }
     return sides;
 }
 
-function buildGraphic(game, hexagonX, hexagonY, sides, polygon, sideLength, x, y){
+function buildGraphic(game, hexagonX, hexagonY, polygon, x, y){
     let graphics = game.add.graphics(hexagonX, hexagonY);
     graphics.beginFill(0xFF33ff);
     graphics.drawPolygon(polygon.points);
@@ -83,70 +82,83 @@ function colourCombinations(colour1, colour2){
     }
 }
 
+function getSideObjects(x, y, sideLength){
+    let corner_vertical = Math.sin(Math.PI/3)*sideLength;
+    let corner_horizontal = Math.cos(Math.PI/3)*sideLength;
+    let points = [
+        {x: -corner_horizontal, y: -corner_vertical},
+        {x: +corner_horizontal, y: -corner_vertical},
+        {x: sideLength, y: 0},
+        {x: +corner_horizontal, y: +corner_vertical},
+        {x: -corner_horizontal, y: +corner_vertical},
+        {x: -sideLength, y: 0},
+    ];
+    let sideObjects = [];
+    for(let i=0; i<points.length;i++){
+        let sideObject = {};
+        sideObject.start = points[i];
+        sideObject.end = points[(i+1)%points.length];
+        sideObject.secondSide = (i + 3)%points.length;
+        let secondHexX = [0, 1, 1, 0, -1, -1];
+        let secondHexY = [-1, -x%2, 1 - x%2, 1, 1-x%2, -x%2];
+        sideObject.secondHex = {x: secondHexX[i], y: secondHexY[i]};
+        sideObjects.push(sideObject);
+    }
+    return sideObjects;
+}
+
 function drawLines(game, hexagons){
+    let lineGraphics = [];
     for(let x = 0; x < hexagons.length; x++){
         for(let y = 0; y < hexagons[x].length; y++){
             let centerHexagon = hexagons[x][y];
-            let corner_vertical = Math.sin(Math.PI/3)*centerHexagon.sideLength;
-            let corner_horizontal = Math.cos(Math.PI/3)*centerHexagon.sideLength;
-            let lineGraphics = game.add.graphics(centerHexagon.graphic.x,centerHexagon.graphic.y);
-            let sideObjects = [
-                {
-                    start: {x: -corner_horizontal, y: -corner_vertical},
-                    end: {x: +corner_horizontal, y: -corner_vertical},
-                    secondHex: {x: 0,  y: -1},
-                    secondSide: 3
-                },
-                {
-                    start: {x: +corner_horizontal,  y: -corner_vertical},
-                    end: {x: centerHexagon.sideLength, y: 0},
-                    secondHex: {x: +1, y: x%2},
-                    secondSide: 4
-                },
-                {
-                    start: {x: centerHexagon.sideLength, y: 0},
-                    end: {x: +corner_horizontal, y: +corner_vertical},
-                    secondHex: {x: +1, y: 1- x%2},
-                    secondSide: 5
-                },
-                {
-                    start: {x: +corner_horizontal, y: +corner_vertical},
-                    end: {x: -corner_horizontal, y: +corner_vertical},
-                    secondHex: {x: +0, y: +1},
-                    secondSide: 0
-                },
-                {
-                    start: {x: -corner_horizontal,  y: +corner_vertical},
-                    end: {x: -centerHexagon.sideLength, y: 0},
-                    secondHex: {x: -1, y: 1- x%2},
-                    secondSide: 1
-                },
-                {
-                    start: {x: -centerHexagon.sideLength, y: 0},
-                    end: {x: -corner_horizontal, y: -corner_vertical},
-                    secondHex: {x: -1, y: x%2},
-                    secondSide: 2
-                }
-            ];
-            let teamColors = [colours.get("red"), colours.get("yellow"), colours.get("blue")];
+            lineGraphics.push(drawCenterLines(game, centerHexagon, x, y, lineGraphics));
+            let lineGraphic = game.add.graphics(centerHexagon.graphic.x,centerHexagon.graphic.y);
+            let sideObjects = getSideObjects(x, y, centerHexagon.sideLength);
             for(let sideNumber = 0; sideNumber < 6; sideNumber++){
                 let sideObject = sideObjects[sideNumber];
                 let hexagon2Coordinates = {"x": x + sideObject.secondHex.x, "y": y + sideObject.secondHex.y};
-                let colour1 = teams[centerHexagon.sides.get(sideNumber)].colour;
-                if(hexagon2Coordinates.x < 0 || hexagon2Coordinates.x >= hexagons.length ||
-                   hexagon2Coordinates.y < 0 || hexagon2Coordinates.y >= hexagons[x].length){
-                   lineGraphics.lineStyle(2, colour1, 100);
+                let colour1 = teams[centerHexagon.sides[sideNumber]].colour;
+                let hexagon2Exists = !(hexagon2Coordinates.x < 0 || hexagon2Coordinates.x >= hexagons.length || hexagon2Coordinates.y < 0 || hexagon2Coordinates.y >= hexagons[x].length);
+                if(!hexagon2Exists){
+                   lineGraphic.lineStyle(2, colour1, 100);
                }else if(sideNumber < 3){
                     let hexagon2 = hexagons[hexagon2Coordinates.x][hexagon2Coordinates.y];
-                    let colour2 = teams[hexagon2.sides.get(sideObject.secondSide)].colour;
-                    lineGraphics.lineStyle(2, colourCombinations(colour1, colour2), 100);
+                    let colour2 = teams[hexagon2.sides[sideObject.secondSide]].colour;
+                    lineGraphic.lineStyle(2, colourCombinations(colour1, colour2), 100);
                 }else{
                     continue;
                 }
-                lineGraphics.moveTo(sideObject.start.x, sideObject.start.y);
-                lineGraphics.lineTo(sideObject.end.x, sideObject.end.y);
+                lineGraphic.moveTo(sideObject.start.x, sideObject.start.y);
+                lineGraphic.lineTo(sideObject.end.x, sideObject.end.y);
             }
+            lineGraphics.push(lineGraphic);
         }
+    }
+    return lineGraphics;
+}
+
+function drawCenterLines(game, centerHexagon, x, y){
+    let lineGraphic = game.add.graphics(centerHexagon.graphic.x,centerHexagon.graphic.y);
+    let sideObjects = getSideObjects(x, y, centerHexagon.sideLength * 0.6);
+    for(let sideNumber = 0; sideNumber < 6; sideNumber++){
+        let sideObject = sideObjects[sideNumber];
+        let colour = teams[centerHexagon.sides[sideNumber]].colour;
+        lineGraphic.lineStyle(2, colour, 100);
+        lineGraphic.moveTo(sideObject.start.x, sideObject.start.y);
+        lineGraphic.lineTo(sideObject.end.x, sideObject.end.y);
+    }
+    return lineGraphic;
+}
+
+function rotateHexagon(hexagon, amount){
+    amount = amount % 6;
+    //for anti-clockwise
+    if(amount < 0){
+        amount = 6-amount;
+    }
+    for(let i=0;i<amount;i++){
+        hexagon.sides.push(hexagon.sides.pop());
     }
 }
 
@@ -156,18 +168,19 @@ window.onload = function() {
 
 	let gridSizeX = 10;
 	let gridSizeY = 12;
+    let lineGraphics = [];
+    let hexagons = [];
 
 	function onPreload() {}
 
 	function onCreate() {
 	    game.stage.backgroundColor = "#ffffff";
-        let hexagons = [];
 	    for(let x = 0; x < gridSizeX; x++){
             let current_row = [];
             hexagons.push(current_row);
 			for(let y = 0; y < gridSizeY; y++){
                 let sideLength = 20;
-                let {polygon, width: hexagonWidth, height: hexagonHeight} = build_hexagon_polygon(sideLength);
+                let {polygon, height: hexagonHeight} = build_hexagon_polygon(sideLength);
                 //plus ones so we don't get cut off by edge of map
                 let hexagonX = sideLength*(x+1)*1.5;
                 let hexagonY = hexagonHeight*(y+1);
@@ -177,7 +190,7 @@ window.onload = function() {
                 let sides = asign_sides();
                 let hexagon = {
                     sideLength: sideLength,
-                    graphic: buildGraphic(game, hexagonX, hexagonY, sides, polygon, sideLength, x, y),
+                    graphic: buildGraphic(game, hexagonX, hexagonY, polygon, x, y),
                     polygon: polygon,
                     x: x,
                     y: y,
